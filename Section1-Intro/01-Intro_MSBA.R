edf98913b5761bd152f08e9c95db1a91d1297457
CaptainNemo <rohitashwachaks@gmail.com>
2021-07-15 01:03:52 -0500

    Update 01-Intro_MSBA.R

--- Section1-Intro/01-Intro_MSBA.R
+++ Section1-Intro/01-Intro_MSBA.R
@@ -2,19 +2,28 @@ library(MASS) ## a library of example datasets
 library(class) ## a library with lots of classification tools
 library(kknn) ## knn library
 
-
+# Adds Boston dataset to the Global Scope.(?)
 attach(Boston)
-n = dim(Boston)[1]
+n <- dim(Boston)[1]
 
-plot(lstat,medv)
+# Plotting %age of pop. from Lower economic status v/s Median Property Value
+plot(lstat,medv,
+     xlab = "%age Pop. from Lower economic status",
+     ylab = "Median Property Value",
+     main = "Relationship b/w Property Value & Econ. Status of Pop.")
 
-train = data.frame(lstat,medv)
-test = data.frame(lstat,medv)
+train <- data.frame(lstat,medv)
+test <- data.frame(lstat,medv)
+
+# Sorting test w.r.t Column 1
+# Ordering so that the line we get is not zig-zig all over the screen
 ind = order(test[,1])
 test =test[ind,]
 
+# Initialising MSE
 MSE = NULL
 
+# Trying out different neighboyrhood size
 kk = c(2,10,50,100,150,200,250,300,400,505)
 
 for(i in kk){
@@ -48,17 +57,13 @@ cat ("Press [enter] to continue")
 line <- readline()
 }
 
-######################################
-### OUT-OF-SAMPLE Prediction
-######################################
+#-------------OUT-OF-SAMPLE Prediction----------------
 
-train = data.frame(lstat,medv)
-test = data.frame(lstat,medv)
-
+set.seed(121)
 tr = sample(1:506,400)
 
-train = train[tr,]
-test = test[-tr,]
+train = data.frame(lstat,medv)[tr,]
+test = data.frame(lstat,medv)[-tr,]
 
 out_MSE = NULL
 
@@ -70,83 +75,94 @@ aux = mean((test[,2]-near$fitted)^2)
 out_MSE = c(out_MSE,aux)
 }
 
-
+# min(out_MSE) -> Minimum MSE Value
+# which.min(out_MSE) -> Index of Minimum MSE Value
 best = which.min(out_MSE)
 
-plot(log(1/(2:350)),sqrt(out_MSE),xlab="Complexity (log(1/k))",ylab="out-of-sample RMSE",col=4,lwd=2,type="l",cex.lab=1.2)
-text(log(1/best),sqrt(out_MSE[best])+0.3,paste("k=",best),col=2,cex=1.2)
+plot(log(1/(2:350)),sqrt(out_MSE),
+     xlab="Complexity (log(1/k))",
+     ylab="Cross Validation RMSE",
+     main = "Model Performance",
+     col=4,lwd=2,type="l",cex.lab=1.2)
+
+text(log(1/best),sqrt(out_MSE[best])+0.3,paste("k=",best+1),col=2,cex=1.2)
 text(log(1/2),sqrt(out_MSE[2]),paste("k=",2),col=2,cex=1.2)
 text(log(1/354)+0.4,sqrt(out_MSE[345]),paste("k=",345),col=2,cex=1.2)
 
 
-near = kknn(medv~lstat,train,test,k=42,kernel = "rectangular")
+near = kknn(medv~lstat,train,test,k=best+1,kernel = "rectangular")
 
 ind = order(test[,1])
-plot(lstat,medv,main=paste("k=",42),pch=19,cex=0.8,col="darkgray")
+plot(lstat,medv,main=paste("k=",best+1),pch=19,cex=0.8,col="darkgray")
 lines(test[ind,1],near$fitted[ind],col=2,lwd=2)
 
-#########################################
-# leave-one-out cross validation (LOOCV)#
-#########################################
+#------leave-one-out cross validation (LOOCV)-------
 
-train = data.frame(lstat,medv)
-test = data.frame(lstat,medv)
+# Adds Boston dataset to the Global Scope.(?)
+attach(Boston)
+n <- dim(Boston)[1]
 
-
-out_MSE = matrix(0,n,100)
-
-for(j in 1:n){
+#train = data.frame(lstat,medv)
+#test = data.frame(lstat,medv)
 
 
-train_i = train[-j,]
-test_i = test[j,]
+out_MSE <- matrix(0,n,100)
 
-for(i in 1:100){
-
-near = kknn(medv~lstat,train_i,test_i,k=i,kernel = "rectangular")
-aux = mean((test_i[,2]-near$fitted)^2)
-
-out_MSE[j,i] = aux
+for(j in 1:n)
+{
+  train_i <- data.frame(lstat,medv)[-j,]
+  test_i <- data.frame(lstat,medv)[j,]
+  
+  for(i in 1:100)
+  {
+    # medv~lstat: medv is Y, and parameter list is lstat
+    near <- kknn(medv~lstat,train_i,test_i,k=i,kernel = "rectangular")
+    aux <- mean((test_i[,2]-near$fitted)^2)
+    
+    out_MSE[j,i] <- aux
+  }
+  cat(j,'\n') # Print command
 }
-cat(j,'\n')
-}
 
-mMSE = apply(out_MSE,2,mean)
+mMSE <- apply(out_MSE,2,mean)
 
-plot(log(1/(1:100)),sqrt(mMSE),xlab="Complexity (log(1/k))",ylab="out-of-sample RMSE",col=4,lwd=2,type="l",cex.lab=1.2)
+plot(log(1/(1:100)),sqrt(mMSE),
+     xlab="Complexity (log(1/k))",
+     ylab="Cross Validation RMSE",
+     main = "LOOCV",
+     col=4,lwd=2,type="l",cex.lab=1.2)
+
 best = which.min(mMSE)
+
 text(log(1/best),sqrt(mMSE[best])+0.1,paste("k=",best),col=2,cex=1.2)
 text(log(1/2),sqrt(mMSE[2])+0.3,paste("k=",2),col=2,cex=1.2)
 text(log(1/100)+0.4,sqrt(mMSE[100]),paste("k=",100),col=2,cex=1.2)
 
+#------K-fold Cross-Validation-------
 
-
-
-#################################
-# k-fold cross validation       #
-#################################
 kcv = 10
-n0 = round(n/kcv,0)
+# Divide into groups of size n0
+n0 = ceiling(n/kcv,0)
 
 out_MSE = matrix(0,kcv,100)
 
 used = NULL
 set = 1:n
 
-for(j in 1:kcv){
-
+for(j in 1:kcv)
+{
 if(n0<length(set)){val = sample(set,n0)}
 if(n0>=length(set)){val=set}
 
 train_i = train[-val,]
 test_i = test[val,]
 
-for(i in 1:100){
-
-near = kknn(medv~lstat,train_i,test_i,k=i,kernel = "rectangular")
-aux = mean((test_i[,2]-near$fitted)^2)
-
-out_MSE[j,i] = aux
+for(i in 1:100)
+{
+  near = kknn(medv~lstat,train_i,test_i,k=i,kernel = "rectangular")
+  aux = mean((test_i[,2]-near$fitted)^2)
+  
+  out_MSE[j,i] = aux
 }
 
 used = union(used,val)
